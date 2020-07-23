e410ed46221c16e2af2fbf3d0c7cdb5692c83c98
Florian Wessels <flo.wessels@gmail.com>
2020-07-23 09:57:56 +0200

    Initial commit

---
+++ Classes/Domain/Model/Country.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "Static Info Tables (BG)" extension for TYPO3 CMS.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ * Florian Wessels <f.wessels@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
+ */
+
+namespace SJBR\StaticInfoTables\Domain\Model;
+
+class Country extends AbstractEntity
+{
+    /**
+     * @var string
+     */
+    protected $shortNameBg = '';
+
+    /**
+     * @return string
+     */
+    public function getShortNameBg(): string
+    {
+        return $this->shortNameBg;
+    }
+
+    /**
+     * @param string $shortNameBg
+     */
+    public function setShortNameBg(string $shortNameBg): void
+    {
+        $this->shortNameBg = $shortNameBg;
+    }
+}
---
+++ ext_tables.sql
@@ -0,0 +1,3 @@
+CREATE TABLE static_countries (
+  cn_short_bg varchar(100) DEFAULT '' NOT NULL
+);
